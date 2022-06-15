37aee62fe3a89111da4717d6d825a3d22bfdef6b
Pranav Malik <86750720+Pranav2625@users.noreply.github.com>
2022-06-15 06:57:07 +0000

    Added comments for Single Username class

--- main.py
+++ main.py
@@ -40,13 +40,15 @@ class Main_screen:
 
     def two_play():
       print("two players")
+      # get_dble_username = Double_Username(self)
+      # get_dble_username.dble_usrnme_text.configure(text = "Enter in usernames for the two players")
 
     def three_play():
       print("Three players")
 
-  def single_username(self):
-    get_sngle_usernme = Single_Username(self)
-    get_sngle_usernme.sngle_usernme_text.configure(text = "Enter in a username")
+  def single_username(self):                                           # When the single player button is pressed
+    get_sngle_usernme = Single_Username(self)                          # Calls on the Single_Username class
+    get_sngle_usernme.sngle_usernme_text.configure(text = "Enter in a username") # and makes the heading of the window
 
 class Single_Username:
   def __init__(self, partner):
@@ -62,31 +64,33 @@ class Single_Username:
     self.sngle_usr_frame = Frame(self.sngle_usrnme_win, width = 600, height = 600, bg = "maroon")  # Makes the frame for this window
     self.sngle_usr_frame.grid()
 
-    self.sngle_usernme_text = Label(self.sngle_usr_frame, text = "", font = "arial 14 bold", justify = CENTER, 
+    self.sngle_usernme_text = Label(self.sngle_usr_frame, text = "", font = "arial 14 bold", justify = CENTER,  # Adds the heading
                                     width = 40, bg = "orange", wrap = 250)
     self.sngle_usernme_text.grid(row = 0)
 
-    def prnt_sngle_usr():
-      sngle_username = self.sngle_usr_entry_box.get()
-      print("Hello", sngle_username)
+    def prnt_sngle_usr():                                    # Carries out defintion if confirm is pushed
+      sngle_username = self.sngle_usr_entry_box.get()        # Stores the input text from the entry field in this variable as string
+      print("Hello", sngle_username)                         # and prints it out
     
-    self.sngle_usr_entry_box = Entry(self.sngle_usr_frame, width = 20, font = "arial 14")
+    self.sngle_usr_entry_box = Entry(self.sngle_usr_frame, width = 20, font = "arial 14") # Creates the entry box
     self.sngle_usr_entry_box.grid(row = 2, pady = 10)
 
-    self.sngle_usr_confirm = Button(self.sngle_usr_frame, text = "Confirm", bg = "gold", font = "arial 10 bold",
-                                   command = prnt_sngle_usr)
+    self.sngle_usr_confirm = Button(self.sngle_usr_frame, text = "Confirm", bg = "gold", font = "arial 10 bold", # Creates
+                                   command = prnt_sngle_usr)                                                     # confirm button
     self.sngle_usr_confirm.grid(row = 4, pady = 10)
     
-    self.sngle_usr_back = Button(self.sngle_usr_frame, text = "Back", width = 10, bg = "orange", 
+    self.sngle_usr_back = Button(self.sngle_usr_frame, text = "Back", width = 10, bg = "orange",           # Creates back button
                                  font = "arial 10 bold", command = partial(self.close_sngle_usr, partner))
     self.sngle_usr_back.grid(row = 6, pady = 10)
 
-  def close_sngle_usr(self, partner):
-    partner.sngle_button.config(state = NORMAL)
-    self.sngle_usrnme_win.destroy()
+  def close_sngle_usr(self, partner):             # If either the back button or window is closed
+    partner.sngle_button.config(state = NORMAL)   # revert the single player button back to normal
+    self.sngle_usrnme_win.destroy()               # and destroy the window
 
 
-
+# class Double_Username:
+#   def __init__(self, partner):
+#     print("hello")
     
 # main routine:
 if __name__ == "__main__":
